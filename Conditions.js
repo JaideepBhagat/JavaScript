0b694735537f8d6a849c6f9535516942b76d834c
JaideepBhagat <jaideep.bhagat@yahoo.in>
2024-07-20 08:57:53 +0530

    no changes

--- Conditions.js
+++ Conditions.js
@@ -4,7 +4,6 @@ if(!flag) console.log("Condition satisfied")
 else console.log("Condition not satisfied")
 
 x = 10, y =15
-
 if(x > y) console.log("x is greater")
 if(y > x) console.log("y is greater")
 else console.log("x is equal to y")
